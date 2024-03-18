dbc6689c4909ad516d6f610d8070d42a039ae846
carlos-ochoa8 <carlosochoa@tae.capital>
2024-03-18 14:12:53 -0600

    add: CORSMiddleware:

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from app.api.routers import api_router
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 fastapi_app = FastAPI(
     title='stationery_api',
     description='stationery API',
     swagger_ui_parameters={'docExpansion': 'None'}
+)
+
+fastapi_app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 fastapi_app.include_router(
